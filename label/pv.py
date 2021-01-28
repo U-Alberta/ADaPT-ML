60c1667b1be2ef37cde63c07ba756ba65bfe3588
Andrea Whittaker <amwhitta@ualberta.ca>
2021-01-28 11:00:46 -0700

    still fixing

--- label/pv.py
+++ label/pv.py
@@ -40,15 +40,15 @@ def main():
         # model.validate_training_data(labeled_train_df, ValueLabel)
         logging.info("Evaluating ...")
         evaluate.lf_summary(train_L, pv_lfs, label_model)
-        if parsed_args.dev_data:
-            # TODO: figure out how do get the label studio project uuid and switch then export the completions
-            # requests.get('http://129.128.215.241:8080/api/project-switch/?project_uuid=32d6ccea-2b41-479c-8272-a4ebfdd1d5aa')
-            dev_df = evaluate.get_dev_df()
-            y_dev = dev_df.gold_label.tolist()
-            if parsed_args.task == 'multiclass':
-                metrics = evaluate.multiclass_summary(train_L, y_dev, label_model, labeled_train_df.label.tolist())
-            elif parsed_args.task == 'multilabel':
-                metrics = evaluate.multilabel_summary(labeled_train_df, label_model)
+        # if parsed_args.dev_data:
+        #     # TODO: figure out how do get the label studio project uuid and switch then export the completions
+        #     # requests.get('http://129.128.215.241:8080/api/project-switch/?project_uuid=32d6ccea-2b41-479c-8272-a4ebfdd1d5aa')
+        #     dev_df = evaluate.get_dev_df()
+        #     y_dev = dev_df.gold_label.tolist()
+        #     if parsed_args.task == 'multiclass':
+        #         metrics = evaluate.multiclass_summary(train_L, y_dev, label_model, labeled_train_df.label.tolist())
+        #     elif parsed_args.task == 'multilabel':
+        #         metrics = evaluate.multilabel_summary(labeled_train_df, label_model)
 
         logging.info("Saving ...")
         input_example = train_L[:5, :]
