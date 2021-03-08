3530ef83ff4ff88896538a855e124c3172c4c3b3
Andrea Whittaker <amwhitta@ualberta.ca>
2021-03-08 16:10:51 -0700

    init deployment

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
-from typing import Optional
-
 from fastapi import FastAPI
+import pickle
+import pandas as pd
 
 app = FastAPI()
+model_path = '/mlruns/1/df8e5c1c720e4376b1d30faf7b060e84/artifacts/mlp/python_model.pkl'
+
+with open(model_path, 'rb') as infile:
+    model = pickle.load(infile)
 
 
 @app.get("/")
@@ -10,6 +14,7 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/predict")
+def predict(id_df: pd.DataFrame):
+    result_df = model.predict(id_df)
+    return result_df.to_json()
