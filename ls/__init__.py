4d691a80c917399a9dbae3436fc4c9ca6f252ef7
Andrea Whittaker <amwhitta@ualberta.ca>
2021-03-11 12:02:46 -0700

    logging

--- ls/__init__.py
+++ ls/__init__.py
@@ -2,6 +2,6 @@ import os
 import logging
 
 CRATE_DB_IP = os.environ['CRATE_DB_IP']
-PV_TASKS_DIRECTORY = '/pv_tasts'
+PV_TASKS_DIRECTORY = '/pv_tasks'
 LOGGING_FILENAME = '/dev_log.txt'
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO, filename=LOGGING_FILENAME, filemode='w')
