571caa53f5185677d1275a15e812281d18606f89
Andrea Whittaker <amwhitta@ualberta.ca>
2021-03-11 13:30:50 -0700

    fix startup

--- ls/__init__.py
+++ ls/__init__.py
@@ -2,6 +2,8 @@ import os
 import logging
 
 CRATE_DB_IP = os.environ['CRATE_DB_IP']
-PV_TASKS_DIRECTORY = '/pv_tasks'
-LOGGING_FILENAME = '/dev_log.txt'
+LABEL_STUDIO_DIRECTORY = '/label_studio'
+PV_TASKS_DIRECTORY = os.path.join(LABEL_STUDIO_DIRECTORY, 'pv_tasks')
+LOGGING_FILENAME = os.path.join(LABEL_STUDIO_DIRECTORY, 'dev_log.txt')
+
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO, filename=LOGGING_FILENAME, filemode='w')
