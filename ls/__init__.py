75e9485eecad74630b550b546d384051548c1fb7
Andrea Whittaker <amwhitta@ualberta.ca>
2021-06-08 11:54:55 -0600

    finish annotator agreement

--- ls/__init__.py
+++ ls/__init__.py
@@ -6,4 +6,17 @@ LABEL_STUDIO_DIRECTORY = '/label_studio'
 PV_TASKS_DIRECTORY = os.path.join(LABEL_STUDIO_DIRECTORY, 'pv_tasks')
 LOGGING_FILENAME = os.path.join(LABEL_STUDIO_DIRECTORY, 'dev_log.txt')
 
+PV_VOCAB = [
+    "security",
+    "conformity",
+    "tradition",
+    "benevolence",
+    "universalism",
+    "self_direction",
+    "stimulation",
+    "hedonism",
+    "achievement",
+    "power"
+]
+
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO, filename=LOGGING_FILENAME, filemode='w')
