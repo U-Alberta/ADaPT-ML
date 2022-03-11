4b2ce24d4804e0f6fefe6845ebec4cb93243b73e
Andrea Whittaker <amwhitta@ualberta.ca>
2022-03-10 18:37:03 -0700

    try another way of switching

--- test/switch-daemons.ps1
+++ test/switch-daemons.ps1
@@ -21,23 +21,8 @@
 Write-Host "Set Path in variable"
 $FILE = "C:\ProgramData\docker\config\daemon.json"
 
-#If the file does not exist, create it.
-if (-not(Test-Path -Path $FILE -PathType Leaf)) {
-     try {
-         $null = New-Item -ItemType File -Path $FILE -Force -ErrorAction Stop
-         Write-Host "The file [$FILE] has been created."
-     }
-     catch {
-         throw $_.Exception.Message
-     }
- }
-# If the file already exists, show the message and do nothing.
- else {
-     Write-Host "Cannot create [$FILE] because a file with that name already exists."
- }
-
-Write-Host "Replace experimental: false to experimental: true"
-(Get-Content -Path $FILE) -replace '"experimental": false','"experimental": true' | Set-Content -Path $FILE
+Write-Host "Copy config file with experimental set to true"
+Copy-Item -Path .\test\daemon.json -Destination $FILE -PassThru
 
 Write-Host "Check the file content"
 type $FILE
